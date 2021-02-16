00eceab348fd2014721086411793fe9d954be448
hellfired <79118083+hellfired@users.noreply.github.com>
2021-02-16 15:39:57 +0200

    Update silent_aim.lua

--- silent_aim.lua
+++ silent_aim.lua
@@ -1,5 +1,3 @@
-local library = loadstring(game:HttpGet("https://github.com/hellfired/libraries/blob/main/1st%20one%20ever"))
-
 -- // Services
 local Players = game:GetService("Players")
 local sGui = game:GetService("StarterGui")
